cf98389d21e3b117a3435a1907693ab82fa52780
EmekaKezie <emeka.kezie@gmail.com>
2023-01-31 20:52:11 +0100

    Inital#1

--- DotnetChatGPTClient/Contants.cs
+++ DotnetChatGPTClient/Contants.cs
@@ -8,7 +8,7 @@ namespace DotnetChatGPTClient
 {
     public class Constants
     {
-        public const string Key = "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";
+        public const string Key = "<YOUR KEY HERE>";
         public const string UrlEndpoint = "https://api.openai.com/v1/completions";
     }
 }
